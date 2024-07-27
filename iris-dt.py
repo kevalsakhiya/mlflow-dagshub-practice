0ad7aa655fb3f5456af32a2d027cbc65c310a15f
Keval <kevalsakhiya@gmail.com>
2024-07-27 11:56:37 +0530

    adding autoscaling with gridsearch

--- iris-dt.py
+++ iris-dt.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
@@ -22,28 +23,41 @@ X = iris.data
 y = iris.target
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+
+
 
 # Train a decision tree model
 mlflow.set_experiment('iris-decision-tree')
 mlflow.autolog()
 
 with mlflow.start_run():
-    max_depth = 5
+    max_depth = 1
     random_state = 42
     clf = DecisionTreeClassifier(max_depth=max_depth,random_state=random_state)
-    clf.fit(X_train, y_train)
+
+    param_grid = {
+    'max_depth' : [1,2,3,4,5,6],
+    'random_state' : [42,45,1,5,6],
+    # 'n_estimator' : [10,20,30,40,100]
+    }
+
+
+    grid_search = GridSearchCV(estimator=clf, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2,)
+
+    grid_search.fit(X_train, y_train)
+    # clf.fit(X_train, y_train)
 
     # Predict the test set results
-    y_pred = clf.predict(X_test)
+    # y_pred = clf.predict(X_test)
 
     # Calculate classification metrics
     print("Classification Report:")
-    classi_report = classification_report(y_test, y_pred)
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred, average='macro')
-    recall = recall_score(y_test, y_pred, average='macro')
-    f1 = f1_score(y_test, y_pred, average='macro')
+    # classi_report = classification_report(y_test, y_pred)
+    # accuracy = accuracy_score(y_test, y_pred)
+    # precision = precision_score(y_test, y_pred, average='macro')
+    # recall = recall_score(y_test, y_pred, average='macro')
+    # f1 = f1_score(y_test, y_pred, average='macro')
     # mlflow.log_metric('accuracy',accuracy)
     # mlflow.log_metric('precision',precision)
     # mlflow.log_metric('recall',recall)
@@ -52,15 +66,15 @@ with mlflow.start_run():
 
 
     # Compute the confusion matrix
-    conf_matrix = confusion_matrix(y_test, y_pred)
+    # conf_matrix = confusion_matrix(y_test, y_pred)
 
     # Plot the confusion matrix using seaborn
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=iris.target_names, yticklabels=iris.target_names)
-    plt.xlabel('Predicted')
-    plt.ylabel('True')
-    plt.title('Confusion Matrix')
-    plt.savefig('confusion_matrix.png')
+    # plt.figure(figsize=(8, 6))
+    # sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=iris.target_names, yticklabels=iris.target_names)
+    # plt.xlabel('Predicted')
+    # plt.ylabel('True')
+    # plt.title('Confusion Matrix')
+    # plt.savefig('confusion_matrix.png')
 
     # artifect
     # mlflow.log_artifact('confusion_matrix.png')
